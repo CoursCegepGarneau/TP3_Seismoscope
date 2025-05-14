7e8ee1102781aad1888f9396794944e220ac9405
Émile Gagnon <emgag008@edu.cegepgarneau.ca>
2025-05-14 16:27:32 -0400

    Ajout SensorAdjustementService

---
+++ Seismoscope/Utils/Services/Interfaces/ISensorAdjustementService.cs
@@ -0,0 +1,14 @@
+﻿using Seismoscope.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seismoscope.Utils.Services.Interfaces
+{
+    public interface ISensorAdjustementService
+    {
+        void EvaluateAndApply(SeismicEvent seismicEvents, Sensor sensor);
+    }
+}
