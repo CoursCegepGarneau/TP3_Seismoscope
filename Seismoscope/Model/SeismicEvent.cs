248cefe522718772b20b271c328e3f339e4fe3c0
Émile Gagnon <emgag008@edu.cegepgarneau.ca>
2025-05-14 16:26:45 -0400

    Ajout class SeismicEvent

---
+++ Seismoscope/Model/SeismicEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seismoscope.Model
+{
+    public class SeismicEvent
+    {
+        public string TypeOnde { get; set; }
+        public double Amplitude { get; set; }
+        public string Note { get; set; }
+    }
+
+}
