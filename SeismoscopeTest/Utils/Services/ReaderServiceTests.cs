63d2048e53b11e60fc4ad72f8c11719a527c4e63
Émile Gagnon <emgag008@edu.cegepgarneau.ca>
2025-05-16 15:32:06 -0400

    Added ReaderSerciceTests

---
+++ SeismoscopeTest/Utils/Services/ReaderServiceTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Seismoscope.Utils.Services;
+
+namespace SeismoscopeTest.Utils.Services
+{
+    public class ReaderServiceTests
+    {
+        [Fact]
+        public void LoadCsv_ShouldLoadLinesCorrectly()
+        {
+            // Arrange
+            var tempFile = Path.GetTempFileName();
+            var lines = new[] { "Line1", "Line2", "Line3" };
+            File.WriteAllLines(tempFile, lines);
+
+            var service = new ReaderService();
+
+            // Act
+            service.LoadCsv(tempFile);
+
+            // Assert
+            Assert.Equal(lines.Length, service.GetTotalLines());
+            Assert.Equal("Line1", service.GetNextLine(0));
+            Assert.Equal("Line3", service.GetNextLine(2));
+            Assert.Null(service.GetNextLine(3));
+
+            // Cleanup
+            File.Delete(tempFile);
+        }
+    }
+
+}
